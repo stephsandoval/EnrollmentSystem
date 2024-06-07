b963109700bb3818d0d03002df5e74fb12567634
stephsandoval <stephsandovalc@gmail.com>
2024-06-07 05:24:16 -0600

    rename files

--- src/main/java/Screens/Main.java
+++ src/main/java/Screens/Main.java
@@ -12,7 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class App extends Application {
+public class Main extends Application {
 
     private Writer writer = Writer.getInstance(GeneralScreen.studentID);
 
